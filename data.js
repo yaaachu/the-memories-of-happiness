b6f87830083d041c0f2a33bc09adff02939440f0
yaaachu <boop932@gmail.com>
2026-01-28 11:57:57 +0800

    Update data.js

--- data.js
+++ data.js
@@ -4,17 +4,35 @@
 
 const posts = [
   {
-    id: "2023-lee-seung-yoon",
+    id: "20230416",
     title: "2023 LEE SEUNG YOON CONCERT <Docking> in Taipei",
     date: "2023 / 04 / 16",
     venue: "信義劇場 Legacy MAX",
-    artist: "李承允 이승윤",
+    artist: "이승윤",
     content: `
 這裡放你的第一篇後記全文。
 
 之後你可以直接在這個 content 裡貼上完整文章，支援換行。
 
 每一篇後記就是一個物件，很好管理。
+    `
+  },
+  {
+    id: "20230715",
+    title: "",
+    date: "2023 / 07 / 15",
+    venue: "",
+    artist: "",
+    content: `
+    `
+  },
+  {
+    id: "20230909",
+    title: "",
+    date: "2023 / 09 / 09",
+    venue: "",
+    artist: "",
+    content: `
     `
   }
 ];
