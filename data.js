0ce2f09004e9d864b52265c9a12ba60ccc5c1fb7
yaaachu <boop932@gmail.com>
2026-01-28 14:35:18 +0800

    Update data.js

--- data.js
+++ data.js
@@ -9,16 +9,13 @@ const posts = [
     date: "2023 / 04 / 16",
     venue: "信義劇場 Legacy MAX",
     content: `
-這裡放你的第一篇後記全文。
-
-之後你可以直接在這個 content 裡貼上完整文章，支援換行。
-
-每一篇後記就是一個物件，很好管理。
+    這裡放你的第一篇後記全文。
+    之後你可以直接在這個 content 裡貼上完整文章，支援換行。
     `
   },
   {
     id: "20230715",
-    title: "PEAK TIME",
+    title: "PEAK TIME CONCER [YOUR TIME] IN TAIPEI",
     date: "2023 / 07 / 15",
     venue: "",
     content: `
@@ -26,8 +23,64 @@ const posts = [
   },
   {
     id: "20230909",
-    title: "FAN CONCERT",
+    title: "M.O.N.T FAN CON in TAIPEI 《Will you be my mint?》",
     date: "2023 / 09 / 09",
+    venue: "",
+    content: `
+    `
+  },
+  {
+    id: "20240504",
+    title: "Restrart in Taipei",
+    date: "2024 / 05 / 04",
+    venue: "",
+    content: `
+    `
+  },
+  {
+    id: "20240510",
+    title: "2024 VANNER 1st CONCERT [THE FLAG:A TO V] IN TAIPEI",
+    date: "2024 / 05 / 10",
+    venue: "",
+    content: `
+    `
+  },
+  {
+    id: "20240524",
+    title: "BEST OF MONT Mini Concert in Taipei",
+    date: "2024 / 05 / 24",
+    venue: "",
+    content: `
+    `
+  },
+  {
+    id: "20250118",
+    title: "M.O.N.T [ON MY WAY TO YOU] CONCERT in Taipei",
+    date: "2025 / 01 / 18",
+    venue: "",
+    content: `
+    `
+  },
+  {
+    id: "20250525",
+    title: "J-hope tour 'HOPE ON THE STAGE' in TAIPEI",
+    date: "2025 / 05 / 25",
+    venue: "",
+    content: `
+    `
+  },
+  {
+    id: "20250906",
+    title: "MONTHLY MONT SPECIAL EDITION IN TAIPEI",
+    date: "2025 / 09 / 06",
+    venue: "",
+    content: `
+    `
+  },
+  {
+    id: "20251225",
+    title: "M.O.N.T WINTER CONCERT 'A DECEMBER TO CELEBRATE' in TAIPEI",
+    date: "2025 / 12 / 25",
     venue: "",
     content: `
     `
