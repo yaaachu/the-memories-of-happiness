fab43fb866410b6ba3afcccfc3a2351e4f14feac
yaaachu <boop932@gmail.com>
2026-02-07 17:07:29 +0800

    Update data.js

--- data.js
+++ data.js
@@ -7,8 +7,7 @@ const posts = [
     id: "20230416",
     title: "2023 LEE SEUNG YOON CONCERT <Docking> in Taipei",
     date: "2023 / 04 / 16",
-    venue: "信義劇場 Legacy MAX",
-    contentFile: "20230416.html"
+    venue: "信義劇場 Legacy MAX"
   },
   {
     id: "20230715",
